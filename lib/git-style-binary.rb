7fa95f1ac9f78886784531647a6ba56971c3fd43
Nate Murray <nate@natemurray.com>
2009-04-17 16:59:35 -0700

    borrowed PP binary finder as stub

--- lib/git-style-binary.rb
+++ lib/git-style-binary.rb
@@ -8,6 +8,19 @@ module GitStyleBinary
   def self.run?
     @run ||= false
   end
+
+  class << self
+    # def list_binaries_for(ty)
+    #   available_binaries_for(ty).join(", ")
+    # end
+    # def available_binaries_for(ty)
+    #   Dir["#{binary_directory}/#{ty}-*"].map {|a| File.basename(a.gsub(/#{ty}-/, '')) }.sort
+    # end
+    # def binary_directory
+    #   "#{::File.dirname(__FILE__)}/../../bin"
+    # end
+  end
+
 end
 
 at_exit do
