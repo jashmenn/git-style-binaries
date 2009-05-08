02a46a8a051c0d47c120c235d2770f556dae7593
Nate Murray <nate@natemurray.com>
2009-05-08 10:27:32 -0700

    fixed a sprintf bug

--- lib/git-style-binary/helpers/name_resolver.rb
+++ lib/git-style-binary/helpers/name_resolver.rb
@@ -69,7 +69,7 @@ module Helpers
       GitStyleBinary.known_commands.collect do |k,cmd| 
         next if k == basename 
         cmd.process_parser!
-        ("%s-%10-s" % [basename, k]).colorize(:blue) + "\n       " + ("%s" % [cmd.short_desc]) + "\n"
+        ("%-s%-10s" % [basename, k]).colorize(:blue) + "\n       " + ("%s" % [cmd.short_desc]) + "\n"
       end.compact.sort
     end
 
